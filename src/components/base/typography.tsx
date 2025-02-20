556e7e80936633059b2974851485fbe689994146
Franka <f.beekman.mmh@gmail.com>
2025-02-20 11:46:48 +0100

    Typography component

---
+++ src/components/base/typography.tsx
@@ -0,0 +1,48 @@
+import { TagTypes, TypographyVariantsType } from "@/types/types"
+import { cls } from "@/utils/functions"
+import { cva } from "class-variance-authority"
+
+interface TypographyProps extends React.HTMLAttributes<HTMLParagraphElement> {
+  tag: TagTypes
+  variant: TypographyVariantsType
+  color?: string
+  link?: boolean
+}
+
+const TypographyStyles = cva(
+  [],
+  {
+    variants: {
+      variant: {
+        body: "font-sans text-base",
+        small: "font-sans text-sm",
+        heading1: "font-heading text-4xl",
+        heading2: "font-heading text-3xl",
+        heading3: "font-heading text-2xl",
+        heading4: "font-heading text-xl",
+        heading5: "font-heading text-lg",
+        heading6: "font-heading text-base",
+      },
+      link: {
+        true: "hover:underline",
+      }
+    }
+  }
+)
+
+export const Typography = ({tag, variant, link, color, className, children}: TypographyProps) => {
+  const Component = tag
+
+  return (
+    <Component className={TypographyStyles({
+      variant,
+      className: cls([
+        className,
+        link,
+        color && `text-${color}`
+      ]),
+    })}>
+      {children}
+    </Component>
+  )
+}
---
+++ src/types/types.ts
@@ -0,0 +1,20 @@
+export type TagTypes = 
+  | "span"
+  | "p"
+  | "h1"
+  | "h2"
+  | "h3"
+  | "h4"
+  | "h5"
+  | "h6"
+  | "small"
+
+export type TypographyVariantsType = 
+  | "body"
+  | "small"
+  | "heading1"
+  | "heading2"
+  | "heading3"
+  | "heading4"
+  | "heading5"
+  | "heading6"
